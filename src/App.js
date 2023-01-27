a7c8a99bb27cd0c1cdb64bbcdc30e693741c03ec
Sangjun Lee <jjojjosj@gmail.com>
2023-01-27 23:02:16 +0900

    move router to index.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,10 @@ import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path={`/movie/:id`} element={<Detail />} />
-        <Route path={`/`} element={<Home />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path={`/movie/:id`} element={<Detail />} />
+      <Route path={`/`} element={<Home />} />
+    </Routes>
   );
 }
 
