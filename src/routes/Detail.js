bdcfeb442af053e06b1bc01a3772d8335b0cba7d
Sangjun Lee <jjojjosj@gmail.com>
2023-01-27 22:12:14 +0900

    영화별 상세 페이지 추가

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,11 +1,37 @@
-import { Link } from "react-router-dom";
+import { getMouseEventOptions } from "@testing-library/user-event/dist/utils";
+import { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
 function Detail() {
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState({});
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovie(json.data.movie);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  console.log(movie);
   return (
     <div>
-      <h1>Detail</h1>
-      <Link to="/">
-        <button>Go Home</button>
-      </Link>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <h1>{movie.title}</h1>
+          <img src={movie.large_cover_image} alt={movie.title} />
+          <h2>{movie.description_intro}</h2>
+          <p>
+            <Link to="/">
+              <button>Go Home</button>
+            </Link>
+          </p>
+        </div>
+      )}
     </div>
   );
 }
